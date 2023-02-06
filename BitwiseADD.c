d1b9eca9260537440d9ed6d0cb66ab68a8873be7
Saira603K <114383320+Saira603K@users.noreply.github.com>
2023-02-06 12:18:05 -0800

    Create BitwiseADD.c

---
+++ BitwiseADD.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+// Use bitwise AND operator b/w two integers
+int main(){
+  int a, b;// declaring variables
+  printf("Enter two numbers: ");//taking user input
+  scanf("%d %d", &a, &b);//storing user input
+  //Using bitwise operator AND
+  printf("Output = %d", a & b);
+return 0;
+  }  
